8806b4a648b10f557a9dd86f3536aefdf0aeb23b
inksne <debanyj23@gmail.com>
2025-01-04 20:54:20 +0300

    add response (отклик) form

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Order
+from .models import Order, Response
 
 class OrderForm(forms.ModelForm):
     class Meta:
@@ -17,3 +17,11 @@ class OrderForm(forms.ModelForm):
             'description': forms.Textarea(attrs={'placeholder': 'Введите описание'}),
             'attachment': forms.ClearableFileInput(),
         }
+
+
+class ResponseForm(forms.ModelForm):
+    class Meta:
+        model = Response
+        fields = ['message']
+        labels = {'message': 'Ваш отклик'}
+        widgets = {'message': forms.Textarea(attrs={'placeholder': 'Введите ваш отклик на заказ'})}
