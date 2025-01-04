1894688ad41b07aa8181891bd9c0ee8d9a15cb16
inksne <debanyj23@gmail.com>
2025-01-04 20:54:44 +0300

    add response (отклик) model

--- main/models.py
+++ main/models.py
@@ -14,7 +14,18 @@ class Order(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     description = models.CharField(max_length=256)
     customer = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
     attachment = models.FileField(upload_to='orders/', null=True, blank=True)
 
     def __str__(self):
         return self.name
+    
+
+class Response(models.Model):
+    order = models.ForeignKey(Order, related_name='responses', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='responses', on_delete=models.CASCADE)
+    message = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Отклик от {self.user.username} на заказ {self.order.name}"
